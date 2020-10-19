470752728ee81f40367e769a3bac322a96927e54
everygit <sbt0198@163.com>
2020-10-19 19:08:56 +0800

    add project simple test

--- src/project.js
+++ src/project.js
@@ -1,3 +1,5 @@
+const {cpdirsSync} = require('@xiaoerr/io');
+const path = require('path');
 
 /**
  * Create a completely independent project based on the project template
@@ -5,6 +7,25 @@
  * @param {string} path The path of the project to be created
  * @param {string} opt options
  */
-module.exports = function(projecttype, path, opt) {
+module.exports = function(projecttype, foldername, spath, opt) {
     console.log('O(∩_∩)O~')
+    switch(projecttype) {
+        case 'simple':
+            createSimpleProject(foldername, spath);
+            break;
+        default:
+            console.log(`ERROR ${projecttype} Type items are not currently supported`.red);
+    }
+}
+
+/**
+ * Create a simple project
+ * @param {string} foldername The name of the folder to create the project
+ * @param {string} path Path to create project
+ */
+function createSimpleProject(foldername, spath) {
+    var sourceFolder = path.resolve(__dirname, '..', 'template', 'project', 'simple');
+    console.log(sourceFolder);
+    var targetFolder = path.resolve('.', spath || '.');
+    console.log(targetFolder);
 }
